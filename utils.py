08160e0c1acbfabc369a4db6cce61ac3c668bd35
juris124 <31391698+juris124@users.noreply.github.com>
2019-12-04 21:57:59 +0200

    garumu constantes

--- utils.py
+++ utils.py
@@ -1,27 +1,37 @@
 import os
 import csv
 
+# pieļaujamais maksimālais testa jomas simbolu nosaukuma garums
+maxJoma=50
+minJoma=5
+# pieļaujamais maksimālais testa nosaukuma simbolu daudzums
+maxTests=150
+minTests=3
+# pieļaujamais maksimālais jautājumu garuma simbolu daudzums
+maxJautGarums=150
+minJautGarums=3
+
 def datnesStruktuurasParbaude(ielades_vieta,fails):
     
     with open(os.path.join(ielades_vieta, fails), newline='', encoding='utf-8') as csvfile:  
         lauki = ['jautajums', 'atbilde1', 'atbilde2', 'atbilde3', 'atbilde4']
         lasitajs = csv.DictReader(csvfile, fieldnames=lauki, delimiter=';')
         testa_joma = next(lasitajs)   
-        if len(testa_joma["jautajums"]) < 5 or len(testa_joma["jautajums"]) > 50: 
+        if len(testa_joma["jautajums"]) < minJoma or len(testa_joma["jautajums"]) > maxJoma: 
             return (False,"Testa jomas nosaukums ir neatbilstošā garumā!")
         #print (testa_joma, testa_joma["atbilde1"])
         if testa_joma["atbilde1"] or testa_joma["atbilde2"] or testa_joma["atbilde3"] or testa_joma["atbilde4"]:            
             return (False,"Nepareiza faila struktūra! Faila 1. rindā jābūt tikai jomas nosaukumam!")
         
         testa_nosaukums = next(lasitajs)
-        if len(testa_nosaukums["jautajums"]) < 3 or len(testa_nosaukums["jautajums"]) > 150: 
+        if len(testa_nosaukums["jautajums"]) < minTests or len(testa_nosaukums["jautajums"]) > maxTests: 
             return (False,"Testa nosaukums ir neatbilstošā garumā!")
         if testa_nosaukums["atbilde1"] or testa_nosaukums["atbilde2"] or testa_nosaukums["atbilde3"]or testa_nosaukums["atbilde4"]: 
             return (False,"Nepareiza faila struktūra! Faila 2. rindā jābūt tikai testa nosaukumam!")
         rindas_skaititajs=3
         for r in lasitajs:
             #print(rindas_skaititajs)
-            if len(r["jautajums"]) < 3 or len(r["jautajums"]) > 100: 
+            if len(r["jautajums"]) < minJautGarums or len(r["jautajums"]) > maxJautGarums: 
                 return (False,f"Jautājuma garums ir neatbilstošs! Problēma rindā nr. {rindas_skaititajs}, jautājums - {r['jautajums']}")
             if len(r["atbilde1"]) == 0 or len(r["atbilde2"]) == 0 or len(r["atbilde3"]) == 0 or len(r["atbilde4"]) == 0: 
                 return (False,f"Nekorektas atbilžu versijas Problēma rindā nr. {rindas_skaititajs}, jautājums - {r['jautajums']}")
