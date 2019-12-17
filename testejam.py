f892530965730a7635f46ef28c5d5f6872e7bfe3
juris124 <31391698+juris124@users.noreply.github.com>
2019-12-18 01:23:41 +0200

    Update testejam.py

--- testejam.py
+++ testejam.py
@@ -1,19 +1,31 @@
 import os
 from github import Github
 
-​def commitFile(fname):
+
+def commitFile(fname):
     #gh_key = os.environ['GH_KEY']
     # using an access token
     # Create token - https://help.github.com/en/github/authenticating-to-github/creating-a-personal-access-token-for-the-command-line
-    g = Github("430482994867d92906262f3fc1055dac3288db0d")
-​
-    repo = g.get_repo("juris124/heroku-tests")
+    g = Github("4c03f031f471a24a49ba2b39488764c6298da10c")
+    repo=g.get_repo("juris124/heroku-tests")
+
+    with open(fname, "r", encoding='utf-8') as f:
+        dircont = repo.get_dir_contents("/static/dati")
+        for fn in dircont:
+            print(fn.path)
+            if fn.path == fname:
+                contents = repo.get_contents(fname)
+                repo.update_file(contents.path, "Faila augšupielāde", f.read(), contents.sha)
+                break
+        else:
+            c = repo.create_file(fname, "test commit from script", f.read())
+            print(c)
+        
     print(repo.name)
-    with open(fname, "r") as f:
+    
         # Docs https://pygithub.readthedocs.io/en/latest/examples/Repository.html#create-a-new-file-in-the-repository
         # Reference https://pygithub.readthedocs.io/en/latest/github_objects/Repository.html#github.Repository.Repository.create_file
-        c = repo.create_file(fname, "test commit from script", f.read())
-    print(c)
-​
-​
-commitFile("testa_fails.txt")
+        
+    
+commitFile("static/dati/20191210_html1.csv")
+ 
